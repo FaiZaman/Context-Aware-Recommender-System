0d621108c91f2e8a7730937ca29ddec64f352460
Faiz <faiz-uz.zaman@dur.ac.uk>
2020-03-13 15:43:19 +0000

    Created basic sign in screen with validation

--- Interface.py
+++ Interface.py
@@ -1,28 +1,39 @@
 import pandas as pd
 import numpy as np
 
-# generate random moods - sad/happy/lazy/active
-
+# reading in data
 main_dataframe = pd.read_csv("dataset/in_car_music.csv", index_col=False, delimiter=",", encoding="utf-8-sig")
 song_dataframe = pd.read_csv("dataset/song_data.csv", index_col=False, delimiter=",", encoding="utf-8-sig")
 music_category_dataframe = pd.read_csv("dataset/music_category.csv", index_col=False, delimiter=",", encoding="utf-8-sig")
 
-mood_types = ['sad', 'happy', 'lazy', 'active']
-
-moods = main_dataframe.loc[:, 'mood']
-for i in range(0, len(moods)):
-    mood = moods[i]
-    if type(mood) == float:
-        main_dataframe.at[i, 'mood'] = np.random.choice(mood_types)
-
-del main_dataframe['DrivingStyle']
-del main_dataframe['landscape']
-del main_dataframe['naturalphenomena ']
-del main_dataframe['RoadType']
-del main_dataframe['sleepiness']
-del main_dataframe['trafficConditions']
-del main_dataframe['weather']
+# get user IDs as list
+user_column = main_dataframe['UserID'].tolist()
+user_id_list = list(dict.fromkeys(user_column))
 
 
-main_dataframe.to_csv("dataset/in_car_music.csv")
-print(main_dataframe.head(20))
+def sign_in():
+
+    # takes user ID as input explicitly and logs them in
+    print("Welcome to the Music Recommender System! Please enter your user ID:")
+
+    while True:
+        try:
+            user_id = int(input())
+            break;
+        except ValueError:
+            print("Invalid format. Please enter an integer and try again.")
+    
+    return user_id
+
+
+def validate_user(user_id):
+    
+    # verifies that given user ID is a valid user
+    if user_id in user_id_list:
+        return True
+    return False
+
+
+user_id = sign_in()
+is_valid_id = validate_user(user_id)
+print(is_valid_id)
---
+++ dataframe_manipulator.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+main_dataframe = pd.read_csv("dataset/in_car_music.csv", index_col=False, delimiter=",", encoding="utf-8-sig")
+song_dataframe = pd.read_csv("dataset/song_data.csv", index_col=False, delimiter=",", encoding="utf-8-sig")
+music_category_dataframe = pd.read_csv("dataset/music_category.csv", index_col=False, delimiter=",", encoding="utf-8-sig")
+
+main_dataframe.to_csv("dataset/in_car_music.csv")
+print(main_dataframe.head(20))
