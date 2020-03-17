09be3f7543e4ff9766d397ee868f7c8371ce32ec
Faiz <faiz-uz.zaman@dur.ac.uk>
2020-03-17 10:56:08 +0000

    Added basic main menu with cleaner interface

--- Interface.py
+++ Interface.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import sys
+from os import system
 from Preprocessor import fetch_data
 from Recommender import get_same_rated_items, compute_similarities, get_user_ratings, \
                         get_user_neighbourhood, compute_recommendations, get_r_best_recommendations
@@ -12,7 +14,33 @@ R = 10  # number of recommendations to output
 threshold = 0.1     # threshold for Filter PoF
 
 main_dataframe, user_id_list, item_id_list = fetch_data()
+system('cls')
 print("Welcome to the Music Recommender System! Please enter your user ID:")
+
+
+# displays menu when user signs in
+def main_menu(user_id):
+
+    print("====== Music Recommender System ======")
+    print("Welcome, User " + str(user_id) + "!\n")
+    print("Press G to generate recommendations")
+    print("Press X to sign out of your account")
+    print("Press Q to quit the Music Recommender System")
+    
+    while True:
+        command = str(input())
+        command = command.upper()
+        if command == 'G' or command == 'X' or command == 'Q':
+            break;
+        else:
+            print("Invalid command. Please try again.")
+    
+    if command == 'G':
+        get_recommendations()
+    elif command == 'X':
+        sign_in()
+    else:
+        sys.exit()
 
 
 def sign_in():
@@ -39,7 +67,7 @@ def validate_user(user_id):
 # display specificed user's personalised recommendations
 def display_recommendations(user_id, predicted_ratings):
 
-    print("Welcome, User " + str(user_id) + "! Your recommendations are:\n")
+    print("Your recommendations are:\n")
 
     # combine to display song titles and artists
     recommendation_template = pd.DataFrame(predicted_ratings, columns=['ItemID', 'Predicted Rating'])
@@ -52,7 +80,10 @@ def display_recommendations(user_id, predicted_ratings):
     recommendations = recommendations.drop('album', 1)
     recommendations = recommendations.drop('category_id', 1)
 
-    recommendations = recommendations[['ItemID', 'title', 'artist', 'Predicted Rating']]    # reorder
+    # reorder columns and sort from most to least recommended
+    recommendations = recommendations[['ItemID', 'title', 'artist', 'Predicted Rating']]
+    recommendations = recommendations.sort_values(by=['Predicted Rating'], ascending=False)
+    recommendations = recommendations.reset_index(drop=True)
 
     print(recommendations)
 
@@ -68,7 +99,7 @@ while not is_valid_id:
         print("The user " + str(user_id) + " does not exist. Please try again:")
 
 
-def main():
+def get_recommendations():
 
      # get cosine similarities between users
     similarity_dict = compute_similarities(user_id)
@@ -84,4 +115,5 @@ def main():
 
     display_recommendations(user_id, r_predicted_ratings)
 
-main()
+main_menu(user_id)
+#get_recommendations()
